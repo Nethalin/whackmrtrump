09d4628813a2d480993c877302542f5de74c99b7
Your Name <administrator@CN-7.lan>
2019-06-12 15:19:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+//empty variable
+let lasthole;
+let timeUp = false;
+let score = 0;
+
+// Generates random time
+function randomTime (min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}//Generates random hole
+function randomHole(holes) {
+ const idx = Math.floor(Math.random() * holes.length);
+ const hole = holes[idx];
+ if (hole === lasthole){
+     console.log("same one")
+    return randomHole(holes);
+}
+lastHole = hole;
+return hole;
+ }
+ //sets time and hole the mole will pop up
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    console.log(time, hole)
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if(!timeUp) peep()
+
+    }, time);
+}
+//resets everything to zero ready to start a new game.
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 10000)
+}
+
+function bonk(e) {
+    score++
+    console.log(e)
+    this.classList.remove("up")
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
